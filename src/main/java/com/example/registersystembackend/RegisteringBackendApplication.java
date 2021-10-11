e9f70098fb441bc0b4e2d77301d88fca5e684284
Luis Ribeiro <luis.ribeiro@stud.fh-campuswien.ac.at>
2021-10-11 22:28:58 +0200

    Project Initialization

---
+++ src/main/java/com/example/registersystembackend/RegisteringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.registersystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisteringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisteringBackendApplication.class, args);
+    }
+
+}
